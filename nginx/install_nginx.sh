4585e45ecadded3470399b627230a4aded4d38a2
Bruno <brunonovo18@gmail.com>
2021-03-20 19:13:17 -0300

    Adding new nginx Helm3 installation

---
+++ nginx/install_nginx.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Source Code
+# https://github.com/kubernetes/ingress-nginx
+
+# Adding and updating Repositories
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+# Setting usefull variables
+NAMESPACE=nginx
+
+# Create Nginx Namespace
+kubectl create ns ${NAMESPACE} || true
+
+# Installing Helm 3 Nginx Ingress Controller
+helm upgrade --install nginx \
+            --namespace ${NAMESPACE} \
+            --set controller.admissionWebhooks.enabled=false \
+            -f values.yaml \
+            ingress-nginx/ingress-nginx
