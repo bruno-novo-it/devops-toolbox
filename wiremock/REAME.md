d811dfe4cddcc59a9ea9d3ed21d422863c105977
bruno-novo-it <brunonovo18@gmail.com>
2021-06-09 18:57:14 -0300

    Adding wiremock docker-compose server

---
+++ wiremock/REAME.md
@@ -0,0 +1,13 @@
+# Running Wiremock
+
+To execute wiremock, just execute:
+
+```sh
+    docker-compose up -d
+```
+
+To stop wiremock server, just execute:
+
+```sh
+    docker-compose down
+```
---
+++ wiremock/wiremock_base_image/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Set `java` command if needed
+if [ "$1" = "" -o "${1:0:1}" = "-" ]; then
+  set -- java $JAVA_OPTS -cp /var/wiremock/lib/*:/var/wiremock/extensions/* com.github.tomakehurst.wiremock.standalone.WireMockServerRunner "$@"
+fi
+
+# allow the container to be started with `-e uid=`
+if [ "$uid" != "" ]; then
+  # Change the ownership of /home/wiremock to $uid
+  chown -R $uid:$uid /home/wiremock
+
+  set -- gosu $uid:$uid "$@"
+fi
+
+exec "$@"
